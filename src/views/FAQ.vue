37baf688d9b71aa9d397212f464543b41ccb1d1e
cetouron <cedric.touron@univ-lyon2.fr>
2021-02-07 22:45:07 +0100

    faq

---
+++ src/views/FAQ.vue
@@ -0,0 +1,86 @@
+<template>
+  <v-container>
+ 
+        <h1>Foire Aux Questions</h1>
+
+<br>
+        <h2>Qu’est-ce que Tut’Up ?</h2>
+        <p>Tut’Up est une plateforme qui permet de mettre en relation des tuteur.ice.s et des élèves -ou parents d’élèves.</p>
+
+        <h2>Comment devenir tuteur.ice ?</h2>
+        <p>En haut à droite, cliquer sur le bouton “s’inscrire” puis cliquer sur “Je suis un.e tuteur.ice”. Ensuite remplissez les différents formulaires qui vous sont présentés.</p>
+
+        <h2>Comment devenir élève/parent d’élève ?</h2>
+        <p>En haut à droite, cliquer sur le bouton “s’inscrire” puis cliquer sur “Je suis un élève/parent d’élève”. Ensuite remplir les différents formulaires qui vous sont présentés.</p>
+
+        <h2>Puis-je m’inscrire comme élève si j’ai moins de 18 ans ?</h2>
+        <p>En raison de cause légal, seul.e des élèves de plus de 18 ans peuvent s’inscrirent sur Tut’up. Si vous avez moins de 18 ans, nous vous invitons à vous rapprocher de vos tuteur.ice.s légaux afin de créer un compte de type “parent d’élève”.</p>
+
+        <h2>Comment trouver un.e tuteur.ice ?</h2>
+        <p>Une fois inscrit.e et votre compte complété, vous pouvez consulter la rubrique “Trouver un.e tuteur.ice”. Depuis celle-ci, faites votre sélection en affinant vos critères de recherche. Vous pourrez alors contacter le.a tuteur.ice qui vous intéresse et lui envoyer un message.</p>
+
+        <h2>A quoi sert l’abonnement “premium” ?</h2>
+        <p>Cette abonnement premium permet à un compte élève / parent d’élève d'apparaître en priorité dans la messagerie des tuteur.ice.s et ceux.lles-ci pourront accéder au profil de la demande premium. Vous aurez alors plus de visibilité et plus de chance de retenir son attention.</p>
+
+        <h2>A combien s’élève la rémunération pour le tutorat ?</h2>
+        <p>Tut’Up ne gère pas la transaction. C’est au.à (la) tuteur.ice et à l’élève de s’arranger. Cependant le profil élève permet de renseigner une proposition de rémunération à l’heure.</p>
+
+        <h2>Comment devenir “premium” ?</h2>
+        <p>Un compte élève peut devenir premium en cliquant sur le bouton “devenir premium” sur son profil ou en cliquant sur la petite étoile à côté de sa photo de profil. Il n’existe pas de compte premium pour les tuteur.ice.s.</p>
+
+        <h2>Comment supprimer mon compte ?</h2>
+        <p>Sur votre profil, cliquez sur le bouton “supprimer mon compte”. Après une validation, celui-ci ainsi que toutes vos données personnelles seront supprimés.</p>
+
+        <h2>Quelles qualifications faut-il pour devenir tuteur.ice ?</h2>
+        <p>Aucune qualification particulière n’est requise. Cependant, vous pouvez renseigner vos différents diplômes et/ou votre parcours scolaire. Si vous avez travaillé dans un domaine se rapprochant de près ou de loin du tutorat (enseignement, animateur, etc.) vous pouvez également le mentionner.</p>
+
+        <h2>Comment sont gérées mes données personnelles ?</h2>
+        <p>Tut’Up ne conserve que les données personnelles nécessaires à la mise en relation et à la mise en avant des demandes et compétences disponibles. Ces données ne seront jamais transmises à des tiers et vous pouvez y accéder ou les supprimer sur simple demande.</p>
+
+
+        <h2>Quelle garantie ai-je en temps qu’élève / parent d’élève ?</h2>
+        <p>Les tuteur.ice.s sont noté.e.s par leurs précédent.e.s élèves via un système de score (sur 5) et de commentaires. De plus les tuteur.ice.s peuvent mettre en avant leur différentes qualifications et expériences</p>
+
+        <h2>Où se passe la séance de tutorat ?</h2>
+        <p>Tut’Up ne gère pas l’organisation de la séance. Cependant il est de coutume que celle-ci se passe chez l’élève et que le.a tuteur.ice se déplace. Vous pouvez en discuter au préalable via la messagerie instantanée du site.</p>
+
+        <h2>J’ai une remarque / une suggestion, comment vous la transmettre ?</h2>
+        <p>Cliquez sur le bouton “nous contacter” en bas de page, puis remplissez le formulaire de contact. Si vous n’êtes pas connecté.e à votre compte, pensez bien à remplir le champs “votre email”. Une réponse vous sera envoyée dès que possible.</p>
+
+
+        <h2>Mon enfant est porteur.se de handicap. Puis-je trouver des tuteur.ice.s formé.e.s/aptes à le.a prendre en charge ?</h2>
+        <p>Les tuteur.ice.s peuvent indiquer s'iels se sentent aptes à prendre en charge des enfants porteur.euse.s de handicap. De plus certain.e.s ont suivi des formations avec des partenaires de Tut’Up afin de répondre au mieux à des profils divers.</p>
+
+        <h2>Comment répondre à un commentaire ?</h2>
+        <p>Vous pouvez répondre à un commentaire sur votre profil. Pour cela, cliquez sur “répondre” en bas de celui-ci. Notez qu’il ne peut y avoir qu’une seule réponse par commentaire, et seulement du.e (la) tuteur.ice concerné.e.</p>
+
+        <h2>Comment signaler un profil ?</h2>
+        <p>En allant sur ce profil, vous pouvez cliquer sur “signaler” afin de signaler un profil. Un champs vous demandera alors de préciser les raisons de votre signalement. Notre équipe de modération prendra ce dernier en charge.</p>
+
+
+
+ 
+
+
+
+
+
+        <h2>Puis-je échanger avec plusieurs tuteur.ice.s simultanément ?</h2>
+        <p>Si vous possédez un compte premium, vous pouvez contacter et discuter avec autant de tuteur.ice.s que vous désirez. Un compte normal ne peux contacter que 2 tuteur.ice.s par jour et doivent attendre que celleux-ci réponde à leur demande de contact pour discuter avec elleux.</p>
+
+        <h2>Comment accéder à une formation en tant que tuteur.ice ?</h2>
+        <p>Tut’Up permet à ses tuteur.ice.s de suivre des formations grâce à ses partenaires. Ces formations, payantes, sont ensuite mis en avant sur leur profil. Il suffit de cliquer sur le bouton “suivre une formation partenaire” afin de découvrir les différentes formations disponibles. Vous pourrez alors vous inscrire avec votre compte Tut’Up sur le site du partenaire.</p>
+
+        <h2>Puis-je inscrire plusieur.e.s élèves sur le même compte ?</h2>
+        <p>Oui, il faut simplement cliquer alors sur “ajouter un.e élève” sur votre profil. Si vous avez plusieur.e.s élèves enregistré.e.s, il vous sera demandé lequel.laquelle est associé.e à une prise de contact.</p>
+
+        <h2>Quel statut légal ai-je en tant que tuteur.ice ?</h2>
+        <p>A partir du moment où vous prenez en charge un.e élève ayant un niveau de formation inférieur au vôtre, vous obtenez de facto le statut de professeur particulier.</p>
+
+        <h2>Comment noter un.e tuteur.ice et lui mettre un commentaire ?</h2>
+        <p>Sur le profil de celui.le-ci, il faut cliquer sur “ajouter un commentaire”. Vous pourrez également lui attribuer une note sur 5. Si vous avez, ainsi que le.a tuteur.ice, cocher l’option “rendez-vous confirmé” dans la messagerie, votre avis apparaîtra comme “séance effectuée” sur la page</p>
+
+        
+
+  </v-container>
+</template>
