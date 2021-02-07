a2d64b91db1de87434e07bf26aaf0135055cea7e
François Seyller <royz3skl@gmail.com>
2021-02-07 19:53:17 +0100

    Design du profil de tuteurs

--- src/components/profilTuteur/ProfilTuteur.vue
+++ src/components/profilTuteur/ProfilTuteur.vue
@@ -1,99 +1,96 @@
 <template>
   <v-row>
     <v-col>
-    <v-card
-      class="mx-auto elevation-20"
-      color="blue"
-      dark
-      align="center"
-      max-width="600"
-      sm="12"
-      md="12"
-      lg="12"
-    >
-      <v-col>
-        <v-avatar size="136" align="center">
-          <img src="img/products/murielle.jpg" style="margin-bottom: 10px" />
-        </v-avatar>
-
-        <br />
-        <h1>Murielle</h1>
-
-        <v-row align="center" justify="center">
-          <v-card-actions class="pa-4" align="center">
-            <v-spacer></v-spacer>
-            <span class="grey--text text--lighten-2 caption mr-1"> (3.2) </span
-            ><!--   -->
-            <v-rating
-              :value="3.2"
-              background-color="black"
-              color="yellow accent-4"
-              readonly
-              dense
-              half-increments
-              hover
-              size="35"
-            ></v-rating>
-          </v-card-actions>
-        </v-row>
-
-        <h4 class="black--text text--lighten-2 mr-1">12 avis</h4>
-
-        <br />
-
+      <v-card
+        class="mx-auto elevation-20"
+        color="blue"
+        dark
+        align="center"
+        max-width="600"
+        sm="12"
+        md="12"
+        lg="12"
+      >
         <v-col>
-          <v-select
-            v-model="value"
-            :items="items"
-            chips
-            label="Matières"
-            multiple
-            outlined
-          ></v-select>
+          <v-avatar size="136" align="center">
+            <img src="img/products/murielle.jpg" style="margin-bottom: 10px" />
+          </v-avatar>
+
+          <br />
+          <h1>Murielle</h1>
+
+          <v-row align="center" justify="center">
+            <v-card-actions class="pa-4" align="center">
+              <v-spacer></v-spacer>
+              <span class="grey--text text--lighten-2 caption mr-1">
+                (3.2) </span
+              ><!--   -->
+              <v-rating
+                :value="3.2"
+                background-color="black"
+                color="yellow accent-4"
+                readonly
+                dense
+                half-increments
+                hover
+                size="35"
+              ></v-rating>
+            </v-card-actions>
+          </v-row>
+
+          <h4 class="black--text text--lighten-2 mr-1">12 avis</h4>
+
+          <br />
+
+          <v-col>
+            <v-select
+              v-model="value"
+              :items="items"
+              chips
+              label="Matières"
+              multiple
+              outlined
+            ></v-select>
+          </v-col>
+
+          <v-col cols="12" class="text-center">
+            <v-textarea
+              v-model="bio"
+              auto-grow
+              filled
+              color="deep-purple"
+              label="Ma bio"
+              rows="1"
+              max-widht="200"
+            ></v-textarea>
+
+            <v-textarea
+              v-model="formation"
+              auto-grow
+              filled
+              color="deep-purple"
+              label="Mes formations"
+              rows="1"
+            ></v-textarea>
+
+            <v-textarea
+              v-model="autre"
+              auto-grow
+              filled
+              color="deep-purple"
+              label="Autre"
+              rows="1"
+            ></v-textarea>
+          </v-col>
         </v-col>
+      </v-card>
+    </v-col>
+    <v-col>
+      <h2>Commentaires</h2>
 
-        <v-col cols="12" class="text-center">
-          <v-textarea
-            v-model="bio"
-            auto-grow
-            filled
-            color="deep-purple"
-            label="Ma bio"
-            rows="1"
-            max-widht="200"
-          ></v-textarea>
-
-          <v-textarea
-            v-model="formation"
-            auto-grow
-            filled
-            color="deep-purple"
-            label="Mes formations"
-            rows="1"
-          ></v-textarea>
-
-          <v-textarea
-            v-model="autre"
-            auto-grow
-            filled
-            color="deep-purple"
-            label="Autre"
-            rows="1"
-          ></v-textarea>
-        </v-col>
-      </v-col>
-    </v-card>
-  
-
-</v-col>
-<v-col>
-
-<h2>Commentaires</h2>
-
-<div v-for="messages in comm"
-          :key="messages.id">
+      <div v-for="messages in comm" :key="messages.id">
         <v-card
-          class="mx-auto elevation-20"
+          class="mx-auto elevation-20 couleurPrimaire comProfilTuteur"
           color="white"
           dark
           align="left"
@@ -104,29 +101,30 @@
             align="right"
             justify="right"
           >
-          <v-row>
-          <v-col>
-            <span class="font-weight-bold" align="right">{{ messages.id }} vous a noté {{ messages.note }} : </span>
-          </v-col>
+            <v-row>
               <v-col>
-              <v-rating
-              :value='messages.note'
-              background-color="black"
-              color="yellow accent-4"
-              readonly
-              half-increments
-              size="20"
-            ></v-rating> 
+                <span class="font-weight-bold" align="right"
+                  >{{ messages.id }} vous a noté {{ messages.note }} :
+                </span>
               </v-col>
-              </v-row>
+              <v-col>
+                <v-rating
+                  :value="messages.note"
+                  background-color="black"
+                  color="white accent-4"
+                  readonly
+                  half-increments
+                  size="20"
+                ></v-rating>
+              </v-col>
+            </v-row>
             {{ messages.content }}
           </span>
         </v-card>
         <br />
       </div>
-
-</v-col>
-</v-row>
+    </v-col>
+  </v-row>
 </template>
 
 
@@ -148,15 +146,26 @@ export default {
         "Histoire",
         "Géographie",
       ],
-      comm: [ 
-          { id: 'Michel73', content: 'Mon commentaire sur le personne, très bien', note:'4' },
-          { id: 'Julie32', content: 'Super merci', note:'4.5' },
-          { id: 'Corentin', content: 'Bravo pour votre entrain', note:'4' },
-          { id: 'Elise', content: 'THate de reprendre contact pour un cours', note:'5' },
-          { id: 'Edith', content: 'A très vite !', note:'4.5' },
-          { id: 'Nicolas', content: "Merci beaucoup grâce à vous j'ai évolué", note:'3.5' },
-       
-        ],
+      comm: [
+        {
+          id: "Michel73",
+          content: "Mon commentaire sur le personne, très bien",
+          note: "4",
+        },
+        { id: "Julie32", content: "Super merci", note: "4.5" },
+        { id: "Corentin", content: "Bravo pour votre entrain", note: "4" },
+        {
+          id: "Elise",
+          content: "THate de reprendre contact pour un cours",
+          note: "5",
+        },
+        { id: "Edith", content: "A très vite !", note: "4.5" },
+        {
+          id: "Nicolas",
+          content: "Merci beaucoup grâce à vous j'ai évolué",
+          note: "3.5",
+        },
+      ],
       value: ["Mathémathique", "Italien", "Physique-Chimie", "Géographie"],
     };
   },
